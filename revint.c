51c4f8287aa136ddcd96177628fff9991ab21696
Madhan-Eshwar <mailtomadhan95@gmail.com>
2016-08-13 14:22:13 +0530

    Update revint.c

--- revint.c
+++ revint.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+  int n,a,sum=0;
+  scanf("%d",&n);
+  while(n!=0)
+  {
+    a=n%10;
+    sum=(sum*10)+a;
+    n=n/10;
+  }
+  printf("%d",sum);
+  return 0;
+}
 
